110d255ac141fe72dc8b09107d2d3ff4c81a17c8
Felipe Tadeu <felipetadeumaiachagas@gmail.com>
2023-08-16 21:34:49 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+import { GraphPageComponent } from './pages/graph-page/graph-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GraphPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+var angularMaterialModules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: angularMaterialModules,
+  exports: angularMaterialModules
+})
+export class AngularMaterialModule { }
